03159e39cacedc44d6298ba4835e9ffd20ab4921
kang <abcnt@naver.com>
2019-02-28 13:53:02 +0900

    ui 컴포넌트 컨트롤러에 연결, MediaPlayer라이브러리 임포트, 관련 변수 작성
    ui 컴포넌트 컨트롤러에 연결, MediaPlayer라이브러리 임포트, 관련 변수 작성

--- MediaPlayer/ViewController.m
+++ MediaPlayer/ViewController.m
@@ -4,11 +4,24 @@
 //
 //  Created by 위피아 on 28/02/2019.
 //  Copyright © 2019 위피아. All rights reserved.
-//
 
+
+//MediaPlayer 임포트!
+#import <MediaPlayer/MediaPlayer.h>
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (strong,nonatomic) MPMediaPickerController *mpPickerVC;                       //픽커 컨트롤러
+@property (strong,nonatomic) MPMediaItemCollection *slctitems;                              //선택된 아이템
+@property (strong,nonatomic) MPMusicPlayerController *appMusicPlayer;               //플레이어
+
+
+@property (weak, nonatomic) IBOutlet UITextField *artistaSearchBar;
+@property (weak, nonatomic) IBOutlet UITextView *soundsTV;
+@property (weak, nonatomic) IBOutlet UIButton *previousBtn;
+@property (weak, nonatomic) IBOutlet UIButton *playBtn;
+@property (weak, nonatomic) IBOutlet UIButton *nextBtn;
 
 @end
 
@@ -17,6 +30,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+
+- (IBAction)onPick:(id)sender {
+}
+
+- (IBAction)onQuery:(id)sender {
+}
+
+- (IBAction)onPrevious:(id)sender {
+}
+- (IBAction)onPlay:(id)sender {
+}
+- (IBAction)onNext:(id)sender {
 }
 
 
